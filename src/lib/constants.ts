b98d451c719ace925f72fffcc4eac711af7502f1
explorewithrs007 <156011575+explorewithrs007@users.noreply.github.com>
2026-05-19 22:16:57 +0530

    Delete src directory

--- src/lib/constants.ts
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = "WhatsApp Platform";
-
-export const WORKSPACE_USER = {
-  email: "aarav@pixelotech.com",
-  name: "Aarav Mehta",
-  role: "Workspace Admin",
-  initials: "AM",
-};
